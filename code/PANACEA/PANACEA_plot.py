9f29e61c60665fb732ad31df0aa28e457482937c
Nerissa <115818450+NerissaX@users.noreply.github.com>
2023-05-25 07:24:34 +0800

    Update PANACEA_plot.py

--- code/PANACEA/PANACEA_plot.py
+++ code/PANACEA/PANACEA_plot.py
@@ -3,18 +3,20 @@ from matplotlib.ticker import FuncFormatter
 import numpy as np
 from PANACEA_constant import *
 
+
 plt.rc('font', size=12)  # controls default text size
-plt.rc('axes', titlesize=12)  # fontsize of the title
+plt.rc('axes', titlesize=14)  # fontsize of the title
 plt.rc('axes', labelsize=12)  # fontsize of the x and y labels
 plt.rc('xtick', labelsize=12)  # fontsize of the x tick labels
 plt.rc('ytick', labelsize=12)  # fontsize of the y tick labels
 plt.rc('legend', fontsize=12)  # fontsize of the legend
-plt.rcParams['axes.labelsize'] = 12
-plt.rcParams['axes.titlesize'] = 14
 plt.rcParams['figure.dpi'] = 600
 
 
 # plot the distribution of ppr, dppr, PEN-distance (featurename)
+# df: a dataframe of a specific feature of the network
+# cancername: string: the name of cancer (e.g.:"Breast Cancer")
+# featurename: string: "ppr", "PEN-distance", etc.
 def plot_features(df, cancername, nodetype, featurename):
     df.stack().plot.hist(bins=50, edgecolor='black')
     plt.yscale('log')
@@ -22,11 +24,14 @@ def plot_features(df, cancername, nodetype, featurename):
     plt.xlim(mn, mx)
     plt.ylabel("Count")
     plt.xlabel(featurename)
-    plt.title(f'{cancername}',weight = 'bold')
+    plt.title(f'{cancername}',weight = 'bold')  # bold the text of the title to make it more visible
     plt.savefig(f'{output_path}//{cancername}_{nodetype}//plot//{cancername}_{featurename} Distribution.png')
     plt.close('all')
 
 
+# plot the delta histogram of a cancer specific signaling network
+# df: dataframe of PEN-diff, ppr-diff, distance-diff
+# fn: the name of the feature: "PEN-diff", "ppr-diff", etc.
 def plot_deltaHist(df, cancer_name, nodetype, fn):
     plt.figure()
     df.plot(x='range',
@@ -43,12 +48,14 @@ def plot_deltaHist(df, cancer_name, nodetype, fn):
     plt.close()
 
 
+# plot the distribution of PEN-diff, ppr-diff or distance-diff
+# ls: a list of PEN-diff, ppr-diff or distance-diff for a cancer network
 def plot_diff_distribution(ls, fn, cancer_name, nodetype):
     plt.figure()  # print the distribution histogram
     # find the maximum size of bin in the distribution and print
     counts, edges, bars = plt.hist(ls, bins=100, edgecolor='black')
     counts = list(counts)
-    print(f'There are {np.nanmax(counts)} combinations in the highest bar of {fn}')
+    print(f'There are {np.nanmax(counts)} combinations in the highest bar of {fn}')  # maximum count of candidates
     plt.xlabel(fn)
     plt.ylabel('count')
     # plt.yscale('log')
