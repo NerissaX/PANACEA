cfc684407f58a1b1501c522ca4055395fa915d72
Nerissa <115818450+NerissaX@users.noreply.github.com>
2024-11-20 18:29:40 +0800

    Update PANACEA.py

--- code/PANACEA/PANACEA.py
+++ code/PANACEA/PANACEA.py
@@ -1,71 +1,71 @@
-from PANACEA_Construct import *
-from PANACEA_Profiling import delta_histogram
-from PANACEA_PEN_diff import PEN_diff
-from PANACEA_constant import *
+from Panacea_Construct import *
+from Panacea_profiling_comb import delta_histogram
+from Panacea_Pen_diff import PEN_diff
+from Panacea_constant import *
 
 
-def panacea(cancer_name, k, cancer_genes, target1, nodetype):
-    # create the cancer signaling network using construcNetwork
-    cancer_network = constructNetwork(cancer_genes, nodetype, target1, whole_signaling, cancer_name)
-    # some targets may not in the signaling network because the human signaling network and drug targets are from
-    # different data sources, we need to remove them first
-    target_in_network = target1.copy()
-    t_in_network = 0
-    for u in target1:
-        if u not in cancer_network.nodes():
-            target_in_network.remove(u)
-        else:
-            t_in_network += 1
+def panacea(cancer_name, k, cancer_genes, targets, nodetype, cancer_network):
+    """
+    Perform analysis for a given cancer subtype.
 
-    # candidate3 is the list of all nodes in the cancer signaling network
-    candidate3 = list(cancer_network.nodes())
+    Parameters:
+    - cancer_name: str, name of the cancer subtype
+    - k: int, size of the k-set
+    - cancer_genes: list, genes associated with the cancer
+    - targets: list, known drug targets
+    - nodetype: str, type of nodes to use (e.g., 'oncogenes')
+    - cancer_network: networkx.Graph, the cancer signaling network
+    """
 
-    # compute PEN_diff, ppr_diff, distance_diff
-    # this function will take several hours for each cancer network
-    PEN_diff(candidate3, k, cancer_genes, nodetype, target1, cancer_network, cancer_name)
+    # Remove targets not present in the cancer network
+    targets_in_network = [t for t in targets if t in cancer_network.nodes()]
+    num_targets_in_network = len(targets_in_network)
 
-    t3_in_network = 0
-    for u in candidate3:
-        if u in target1:
-            t3_in_network += 1
+    # Get list of all nodes in the cancer network
+    candidate_nodes = list(cancer_network.nodes())
 
-    # print basic information about the cancer
-    with open(f'{output_path}//{cancer_name}_{nodetype}//{cancer_name}_{nodetype}_basic_info.csv', 'w') as f:
-        f.write('Cancer Subtype:{}'.format(cancer_name))
-        f.write('\n')
-        f.write('k-set:{}'.format(k))
-        f.write('\n')
-        f.write('There are {} known targets for this cancer subtype'.format(len(target1)))
-        f.write('\n')
-        f.write('There are {} known targets in the cancer network'.format(t_in_network))
-        f.write('\n')
-        f.write('Number of nodes in signaling network:{}'.format(nx.number_of_nodes(whole_signaling)))
-        f.write('\n')
-        f.write('Number of nodes in the cancer network:{}'.format(nx.number_of_nodes(cancer_network)))
-        f.write('\n')
-        f.write('\n')
-    f.close()
-    # find the delta histogram and range constraint of PEN-diff, ppr-diff, average closeness and distance-diff
+    # Compute PEN_diff, ppr_diff, and distance_diff
+    # Note: This function may take several hours for each cancer network
+    PEN_diff(candidate_nodes, k, cancer_genes, nodetype, targets, cancer_network, cancer_name)
+
+    # Write basic information about the cancer to a CSV file
+    output_file = f'{output_path}//{cancer_name}_{nodetype}//{cancer_name}_{nodetype}_basic_info.csv'
+    with open(output_file, 'w') as f:
+        f.write(f'Cancer Subtype:{cancer_name}\n')
+        f.write(f'k-set:{k}\n')
+        f.write(f'There are {len(targets)} known targets for this cancer subtype\n')
+        f.write(f'There are {num_targets_in_network} known targets in the cancer network\n')
+        f.write(f'Number of nodes in signaling network:{nx.number_of_nodes(whole_signaling)}\n')
+        f.write(f'Number of nodes in the cancer network:{nx.number_of_nodes(cancer_network)}\n\n')
+
+    # Find the delta histogram and range constraints
     deltamin, deltamax = delta_histogram(cancer_name, nodetype, k, bucket_no=5)
 
 
 if __name__ == "__main__":
-    nodetype = 'oncogenes'  # the user can choose other cancer nodes like Biomarkers, disease nodes
+    nodetype = 'oncogenes'  # You can choose other node types like 'Biomarkers' or 'disease nodes'
     k = 2
-    # prostate cancer
-    # use find_subgene to find oncogenes, target genes and non-taragets of a specific cancer
-    pc_onco1, pc_target1, pc_nontarget1 = find_subgene(whole_signaling, 'prostate', 'Prostate Cancer')
-    # if a user wants to use other cancer genes instead of oncogenes, replace pc_onco1 in the input with other gene list
-    panacea('Prostate Cancer', k, pc_onco1, pc_target1, nodetype)
 
-    # breast cancer
-    bc_onco1, bc_target1, bc_nontarget1 = find_subgene(whole_signaling, 'breast', 'Breast Cancer')
-    panacea('Breast Cancer', k, bc_onco1, bc_target1, nodetype)
+    # List of cancer types to analyze
+    cancers = [
+        ('prostate', 'Prostate Cancer'),
+        ('breast', 'Breast Cancer'),
+        ('bladder', 'Bladder Cancer'),
+        ('colorectal', 'Colorectal Cancer')
+    ]
 
-    # bladder cancer
-    blc_onco1, blc_target1, blc_nontarget1 = find_subgene(whole_signaling, 'bladder', 'Bladder Cancer')
-    panacea('Bladder Cancer', k, blc_onco1, blc_target1, nodetype)
+    # Analyze each cancer type
+    for cancer_code, cancer_name in cancers:
+        # Find oncogenes, target genes, and non-targets for the cancer
+        cancer_oncogenes, cancer_targets, cancer_nontargets = find_subgene(
+            whole_signaling, cancer_code, cancer_name
+        )
 
-    # colorectal cancer
-    cc_onco1, cc_target1, cc_nontarget1 = find_subgene(whole_signaling, 'colorectal', 'Colorectal Cancer')
-    panacea('Colorectal Cancer', k, cc_onco1, cc_target1, nodetype)
+        # Create the cancer signaling network
+        cancer_network = constructNetwork(
+            cancer_oncogenes, nodetype, cancer_targets, whole_signaling, cancer_name
+        )
+        print("construct network done")
+
+        # Analyze the cancer network
+        panacea(cancer_name, k, cancer_oncogenes, cancer_targets, nodetype, cancer_network)
